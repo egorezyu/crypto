84fd6dc8be0087602381d71133a682acf00bb6f6
egor <egorbiceps228@mail.ru>
2022-06-29 20:09:14 +0300

    working with button

--- TheNewBegining2.0storyBoard/Controllers/PortfolioViewController.swift
+++ TheNewBegining2.0storyBoard/Controllers/PortfolioViewController.swift
@@ -12,10 +12,34 @@ class PortfolioViewController: UIViewController {
     var coins : [Coin]!
     var arrOfCoinChoseViews : [CoinChooseView] = []
     private var currentChose = ""
+    private var textFieldForAmount : UITextField = UITextField()
+    private var addCoinButton : UIButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .backForCoin
+        textFieldForAmount.placeholder =  "Please enter amount of coins"
+        textFieldForAmount.font = UIFont.boldSystemFont(ofSize: 20)
+        textFieldForAmount.textAlignment = .center
+        textFieldForAmount.backgroundColor = .back
+        textFieldForAmount.layer.cornerRadius = 20
+        textFieldForAmount.layer.borderColor = UIColor.cyan.cgColor
+        textFieldForAmount.layer.borderWidth = 3
+        textFieldForAmount.keyboardType = .numberPad
+        textFieldForAmount.isHidden = true
+        textFieldForAmount.addTarget(self, action: #selector(showButton(_:)), for: .allEditingEvents)
+        view.addSubview(textFieldForAmount)
+        addCoinButton.setTitle("Add to portfolio", for: .normal)
+        addCoinButton.setTitleColor(.cyan, for: .normal)
+        addCoinButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 25)
+        addCoinButton.layer.borderColor = UIColor.cyan.cgColor
+        addCoinButton.layer.borderWidth = 3
+        addCoinButton.layer.cornerRadius = 15
+        addCoinButton.isHidden = true
+        view.addSubview(addCoinButton)
+        
+        
+        
 //        print(coins.count)
         
 //        coinChose.addTarget(self, action: #selector(onClickEvent(sender:)), for: .touchUpInside)
@@ -32,12 +56,21 @@ class PortfolioViewController: UIViewController {
         scroll.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         scroll.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         scroll.heightAnchor.constraint(equalToConstant: view.frame.width * 0.3).isActive = true
+        textFieldForAmount.translatesAutoresizingMaskIntoConstraints = false
+        textFieldForAmount.topAnchor.constraint(equalTo: scroll.bottomAnchor,constant: 30).isActive = true
+        textFieldForAmount.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textFieldForAmount.widthAnchor.constraint(equalToConstant: view.frame.width * 0.7).isActive = true
+        textFieldForAmount.heightAnchor.constraint(equalToConstant: view.frame.width * 0.1).isActive = true
+        addCoinButton.translatesAutoresizingMaskIntoConstraints = false
+        addCoinButton.topAnchor.constraint(equalTo: textFieldForAmount.bottomAnchor,constant: 30).isActive = true
+        addCoinButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         let stack = UIStackView()
         scroll.addSubview(stack)
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.topAnchor.constraint(equalTo: scroll.topAnchor).isActive = true
         stack.leadingAnchor.constraint(equalTo: scroll.leadingAnchor).isActive = true
         stack.trailingAnchor.constraint(equalTo: scroll.trailingAnchor).isActive = true
+        
         stack.bottomAnchor.constraint(equalTo: scroll.bottomAnchor).isActive = true
         stack.axis = .horizontal
         stack.distribution = .equalSpacing
@@ -71,6 +104,7 @@ class PortfolioViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @objc func onClickEvent(sender : CoinChooseView){
+        textFieldForAmount.isHidden = false
 //        if (!sender.isToogled){
 //            sender.layer.cornerRadius = 25
 //            sender.layer.borderColor = UIColor.green.cgColor
@@ -105,6 +139,16 @@ class PortfolioViewController: UIViewController {
         }
         
 
+    }
+    @objc func showButton(_ sender : UITextField){
+        if let text = sender.text,!text.isEmpty{
+            addCoinButton.isHidden = false
+            
+        }
+        else{
+            addCoinButton.isHidden = true
+        }
+        
     }
     
 
